bfdc7ffa7e0bb6b31056503e20ba01b8e60a0ec8
heronet <sirat4757@gmail.com>
2021-09-11 01:50:30 +0600

    first commit

---
+++ first_day_at_college.cxx
@@ -0,0 +1,15 @@
+/*			     "My First Day at College"
+
+Today I am here to talk about my first day at my college. The first day at college can mean many different things. The first day to visit the college, The day of admission, First online class, or just randomly going there with a friend :). But I think what we are interested to discuss about is the first day we visit the college to attend a class. So let's talk about that.
+
+Well it's not very interesting for me to begin with. Why? Because my first day at my college is not THE first day at the college. It's tomorrow and I won't be attending. Because I don't have a place there to live yet. I will move as soon as I get one.
+
+With that somehow sad and boring story aside, I am very much interested about what will be MY first day at my college. I am excited to meet my friends and I know many of you are too. You maight ask "Well how do you have friends if you didn't even go to your college?" and that would be a valid question. Despite our college physically being shut, we continued our studies through online classes, spmething we discussed earlier. That is how I made my virual friends. or internet friends as some might say. 
+
+"My suggestion on your first day"
+
+Make new friends
+Talk to people
+Meet your teachers
+
+*/
